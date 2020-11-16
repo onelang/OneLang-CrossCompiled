13f530760b7d5101a40b808f7d81ca0791e706df
KT <koczkatamas@gmail.com>
2020-11-16 01:50:19 +0100

    [Scripts] Also cleanup generated language specific artifacts

--- _scripts/cleanup_generated_code.sh
+++ _scripts/cleanup_generated_code.sh
@@ -4,8 +4,13 @@ rm -rf ../CSharp/Parsers
 rm -rf ../CSharp/StdLib
 rm -rf ../CSharp/Test
 rm -rf ../CSharp/Utils
+rm -rf ../CSharp/bin
+rm -rf ../CSharp/obj
 
 rm -rf ../Java/src/main/java/OneLang
+rm -rf ../Java/.gradle
+rm -rf ../Java/bin
+rm -rf ../Java/build
 
 rm -rf ../PHP/Generator
 rm -rf ../PHP/One
@@ -20,3 +25,6 @@ rm -rf ../Python/OneLang/Parsers
 rm -rf ../Python/OneLang/StdLib
 rm -rf ../Python/OneLang/Test
 rm -rf ../Python/OneLang/Utils
+rm -rf ../Python/__pycache__
+rm -rf ../Python/OneLang/_external/__pycache__
+rm -rf ../Python/onepkg/__pycache__
